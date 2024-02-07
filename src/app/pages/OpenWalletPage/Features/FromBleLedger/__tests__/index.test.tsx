66faeb5ca16f131a2e3f6957a76c2f444cca5507
lubej <matej.lubej@oasisprotocol.org>
2024-02-07 14:27:33 +0100

    Add Ionic Bluetooth UI tests

--- src/app/pages/OpenWalletPage/Features/FromBleLedger/__tests__/index.test.tsx
+++ src/app/pages/OpenWalletPage/Features/FromBleLedger/__tests__/index.test.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react'
 import { render } from '@testing-library/react'
-import { FromUsbLedger } from '..'
+import { FromBleLedger } from '..'
 
 jest.mock('react-redux', () => ({
   useSelector: jest.fn(),
   useDispatch: jest.fn(),
 }))
 
-describe('<FromUsbLedger />', () => {
+describe('<FromBleLedger />', () => {
   it('should render component', () => {
-    const { container } = render(<FromUsbLedger />)
+    const { container } = render(<FromBleLedger />)
 
     expect(container).toMatchSnapshot()
   })
